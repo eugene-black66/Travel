9f437aa0f056300306fb8f66fdfc5ffb0f7f5f47
Eugene <zhenyatina123@gmail.com>
2022-08-25 23:37:00 +0300

    register popup done (animations expected)

--- popups.js
+++ popups.js
@@ -1,13 +1,29 @@
 let loginButton = document.getElementById('headerLoginBtn')
 let loginPopup = document.getElementById('loginPopup')
-let loginOverlay = document.getElementById('loginPopupOverlay')
+let loginOverlay = document.getElementById('PopupOverlay')
 
-loginButton.onclick = function popupShow() {
+let registerPopup = document.getElementById('registerPopup')
+let registerString = document.getElementById('loginFooterRegister')
+let loginString = document.getElementById('registerFooterLogin')
+
+
+loginButton.onclick = function loginPopupOpen() {
     loginPopup.style.display = 'block';
     loginOverlay.style.display = 'block';
 }
 
-loginOverlay.onclick = function popupClose() {
-    loginPopup.style.display = 'none'
-    loginOverlay.style.display = 'none'
+loginOverlay.onclick = function PopupClose() {
+    loginPopup.style.display = 'none';
+    loginOverlay.style.display = 'none';
+    registerPopup.style.display = 'none'
+}
+
+registerString.onclick = function registerPopupOpenLoginPopupClose() {
+    loginPopup.style.display = 'none';
+    registerPopup.style.display = 'block';
+}
+
+loginString.onclick = function registerPopupCloseLoginPopupOpen() {
+    registerPopup.style.display = 'none'
+    loginPopup.style.display = 'block'
 }
